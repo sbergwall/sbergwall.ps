60643a747d2ac486adf235b48baa3492f99116b5
sbergwall <25138273+sbergwall@users.noreply.github.com>
2025-03-08 09:41:55 +0100

    added help for get-maildomaininfo

--- sbergwall.ps/public/Get-MailDomainInfo.ps1
+++ sbergwall.ps/public/Get-MailDomainInfo.ps1
@@ -2,10 +2,46 @@
 #Requires -Modules DnsClientX
 
 function Get-MailDomainInfo {
+    <#
+    .SYNOPSIS
+        Retrieves email-related DNS records for a specified domain.
+
+    .DESCRIPTION
+        The Get-MailDomainInfo function performs DNS lookups to gather various email-related records for a domain,
+        including Autodiscover, DMARC, MX, and SPF records. It uses the DnsClientX module for DNS resolution.
+
+    .PARAMETER domain
+        The domain name to query. Defaults to 'google.com' if not specified.
+
+    .PARAMETER dnsProvider
+        The DNS provider to use for queries. Defaults to 'CloudFlare'.
+
+    .EXAMPLE
+        Get-MailDomainInfo -domain "microsoft.com"
+        Retrieves email DNS records for microsoft.com using the default CloudFlare DNS provider.
+
+    .EXAMPLE
+        Get-MailDomainInfo -domain "github.com" -dnsProvider "Google"
+        Retrieves email DNS records for github.com using Google's DNS provider.
+
+    .NOTES
+        Requires the DnsClientX module to be installed.
+        Author: Simon Bergwall
+        Version: 1.0
+
+    .LINK
+        https://github.com/sbergwall/sbergwall.ps
+
+    .OUTPUTS
+        PSCustomObject containing formatted DNS record information
+    #>
+
     param (
-        $domain = 'google.com',
-        $dnsProvider = 'CloudFlare'
+        $Domain = 'google.com',
+        $DnsProvider = 'CloudFlare'
     )
+
+
 
     $autodiscoverA = (Resolve-Dns -Name "autodiscover.$($domain)" -Type A -DnsProvider $dnsProvider -ErrorAction SilentlyContinue).Data
     $dmarc = (Resolve-Dns -Name "_dmarc.$($domain)" -Type TXT -DnsProvider $dnsProvider -ErrorAction SilentlyContinue ).Data
