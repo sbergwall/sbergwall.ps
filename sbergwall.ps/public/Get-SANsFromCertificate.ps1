6d180635df0d519cdc9b8b43f61b8e90de39f482
sbergwall <25138273+sbergwall@users.noreply.github.com>
2025-10-31 10:57:56 +0100

    Added Certificate functions

---
+++ sbergwall.ps/public/Get-SANsFromCertificate.ps1
@@ -0,0 +1,34 @@
+function Get-SANsFromCertificate {
+    param(
+        [Parameter(Mandatory)]
+        [string]$BinaryCertificate
+    )
+
+    try {
+        # If certificate is in PEM format
+        if ($BinaryCertificate -match 'BEGIN CERTIFICATE') {
+            $clean = ($BinaryCertificate -split "`r?`n" | Where-Object { $_ -notmatch '-----' }) -join ''
+            $bytes = [Convert]::FromBase64String($clean)
+        }
+        else {
+            # Otherwise assume plain base64
+            $bytes = [Convert]::FromBase64String($BinaryCertificate)
+        }
+
+        $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @(, $bytes)
+
+        # Find the SAN extension (OID 2.5.29.17)
+        $sanExt = $cert.Extensions | Where-Object { $_.Oid.Value -eq '2.5.29.17' }
+        if ($sanExt) {
+            # Pretty-print SANs
+            return $sanExt.Format($true)
+        }
+        else {
+            return $null
+        }
+    }
+    catch {
+        Write-Verbose "Failed to decode SANs: $($_.Exception.Message)"
+        return $null
+    }
+}
